31a61ff27e6a929aafdf4bb505fb28a456763004
KengoKato02 <kengo.kato15@gmail.com>
2025-05-27 10:16:36 +0200

    fix typo in description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="coral-bleach-predictor",
     version="0.1.0",
-    description="Aimed to predict coral bleach events triggered via temperatures changes at a global scale within the environmenetal science sector",
+    description="Aimed to predict coral bleach events triggered via temperatures changes at a global scale within the environmental science sector",
     author="Simona Tingleff Kardel <simonakardel@gmail.com>, Wanesa Wintmiller <>, Kengo Reimers Kato <kengo.kato15@gmail.com>",
     packages=find_packages(),  
     install_requires=[         
